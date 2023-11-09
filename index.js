c6a18bbb6fb990e4f8b81365cf69ebcc992430e8
Amrit5s2002 <guramrit1066@gmail.com>
2023-11-10 01:30:58 +0530

    basic layout ready

---
+++ index.js
@@ -0,0 +1,21 @@
+
+// function iconShow(){
+
+//     const skill = document.getElementById('skill1')
+//     const title = document.getElementById('skill-title1')
+//     const p1 = document.getElementById('num-project1')
+
+//     skill.addEventListener('mouseover',()=>{
+//         p1.style.display = "block"
+//         p1.innerHTML = "4 Project"
+//         console.log(p1.style.display)
+//     })
+//     skill.addEventListener('mouseout',()=>{
+//         if(p1.style.display){
+//             console.log(p1.style.display)
+//             p1.style.display = "none"
+//         }
+//     })
+// }
+
+// iconShow()
